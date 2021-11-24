cb05f6ac3842e9ea7f3f1d5ac1cc61610565a7aa
athivvat <athivvat@gmail.com>
2021-11-25 01:56:10 +0700

    Live dashboard

--- dashboard/app.py
+++ dashboard/app.py
@@ -2,6 +2,7 @@ from dotenv import dotenv_values
 import dash
 from dash import dcc
 from dash import html
+from dash.dependencies import Input, Output, State, MATCH, ALL
 import plotly.express as px
 import pandas as pd
 
@@ -14,10 +15,10 @@ df = pd.read_csv('dashboard/assets/cgm48members.csv')
 
 def generate_table(dataframe, max_rows=48):
     headers = []
-     # Add image column
+    # Add image column
     headers.append(html.Th(""))
 
-     # Add column headers
+    # Add column headers
     for col in dataframe.columns:
         if col not in ['Facebook Link', 'Instagram Link']:
             headers.append(html.Th(col))
@@ -28,12 +29,15 @@ def generate_table(dataframe, max_rows=48):
         ),
         html.Tbody([
             html.Tr([
-                html.Td(html.Img(src='assets/images/' + dataframe.iloc[i]['Nickname'].lower() +'.jpg', width=70, height=70, style={'borderRadius': '50%'})),
+                html.Td(html.Img(src='assets/images/' + dataframe.iloc[i]['Nickname'].lower(
+                ) + '.jpg', width=70, height=70, style={'borderRadius': '50%'})),
                 html.Td(dataframe.iloc[i]['Name']),
                 html.Td(dataframe.iloc[i]['Nickname']),
                 html.Td(dataframe.iloc[i]['Age']),
-                html.Td(html.A(children=dataframe.iloc[i]['Facebook'], href=dataframe.iloc[i]['Facebook Link'], target="_blank")),
-                html.Td(html.A(children=dataframe.iloc[i]['Instagram'], href=dataframe.iloc[i]['Instagram Link'], target="_blank")),
+                html.Td(html.A(
+                    children=dataframe.iloc[i]['Facebook'], href=dataframe.iloc[i]['Facebook Link'], target="_blank")),
+                html.Td(html.A(children=dataframe.iloc[i]['Instagram'],
+                        href=dataframe.iloc[i]['Instagram Link'], target="_blank")),
                 html.Td(dataframe.iloc[i]['Twitter Volume']),
                 html.Td(dataframe.iloc[i]['Favorites']),
                 html.Td(dataframe.iloc[i]['Retweets']),
@@ -53,10 +57,32 @@ app.layout = html.Div(children=[
 
     html.Hr(),
 
-    generate_table(df),
+    html.Div(id='live-update'),
 
-    html.P(children='Made with \u2764\ufe0f by 401 Cha-Thai', style={'textAlign': 'center', 'margin': '50px'}),
-])
+    html.P(children='Made with \u2764\ufe0f by 401 Cha-Thai',
+           style={'textAlign': 'center', 'margin': '50px'}),
+
+    dcc.Interval(
+        id='interval-component-slow',
+        interval=1*10000,  # in milliseconds: 10000 ms = 10 sec.
+        n_intervals=0
+    )
+], style={'padding': '20px'})
+
+
+@app.callback(
+    [
+        Output('live-update', 'children')
+    ],
+    [
+        Input('interval-component-slow', 'n_intervals')
+    ]
+)
+def update_table(n):
+    # Loading data from MongoDB
+    df.at[0, 'Twitter Volume'] = n
+    children = [generate_table(df)]
+    return children
 
 
 if __name__ == '__main__':
